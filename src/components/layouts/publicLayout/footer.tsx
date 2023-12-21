e7131d6d9dd1428843de78f26f1d7b5ff99808a8
MuhammadHassanShah <hassan.hameed@netsoltech.com>
2023-12-21 16:53:38 +0500

    Header-footer

--- src/components/layouts/publicLayout/footer.tsx
+++ src/components/layouts/publicLayout/footer.tsx
@@ -1,5 +1,95 @@
 import React from "react";
+import { Input, Button } from "antd";
+import {
+  FacebookOutlined,
+  TwitterOutlined,
+  InstagramOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
+import { MailOutlined, QrcodeOutlined } from "@ant-design/icons";
 
-export default function Footer({}) {
-  return <footer>footer is here</footer>;
+export default function Footer() {
+  return (
+    <footer
+      className="bg-black text-white px-10 pt-5
+     pb-10"
+    >
+      <div className="flex flex-wrap justify-between items-center">
+        <div className="flex flex-col mt-0">
+          <span className="font-bold text-lg mb-2 mt-0">Exclusive</span>
+          <span className="mb-2">Subscribe</span>
+          <span className="mb-2">Get 10% off your first order</span>
+          <Input
+            size="large"
+            placeholder="Enter your email"
+            prefix={<MailOutlined />}
+            className="mb-2"
+          />
+        </div>
+
+        <div className="flex flex-col mt-0">
+          <span className="font-bold text-lg mb-2 mt-0">Support</span>
+          <address className="not-italic mb-2">
+            111 Bijoy Sarani, Dhaka, DH 1515, Bangladesh.
+          </address>
+          <a href="mailto:exclusive@gmail.com" className="mb-2">
+            exclusive@gmail.com
+          </a>
+          <a href="tel:+8801588889999" className="mb-2">
+            +88015-8888-9999
+          </a>
+        </div>
+
+        <div className="flex flex-col mt-0">
+          <span className="font-bold text-lg mb-2 mt-0">Account</span>
+          <a href="/my-account" className="mb-2">
+            My Account
+          </a>
+          <a href="/login" className="mb-2">
+            Login / Register
+          </a>
+          <a href="/cart" className="mb-2">
+            Cart
+          </a>
+          <a href="/wishlist" className="mb-2">
+            Wishlist
+          </a>
+          <a href="/shop" className="mb-2">
+            Shop
+          </a>
+        </div>
+
+        <div className="flex flex-col mt-0">
+          <span className="font-bold text-lg mb-2 mt-0">Quick Link</span>
+          <a href="/privacy-policy" className="mb-2">
+            Privacy Policy
+          </a>
+          <a href="/terms-of-use" className="mb-2">
+            Terms Of Use
+          </a>
+          <a href="/faq" className="mb-2">
+            FAQ
+          </a>
+          <a href="/contact" className="mb-2">
+            Contact
+          </a>
+        </div>
+
+        <div className="flex flex-col mt-0">
+          <span className="font-bold text-lg mb-2 mt-0">Download App</span>
+          <span className="mb-2">Save $3 with App New User Only</span>
+          <Button icon={<QrcodeOutlined />} className="mb-2" />
+          <div className="flex justify-between">
+            <FacebookOutlined className="text-2xl" />
+            <TwitterOutlined className="text-2xl" />
+            <InstagramOutlined className="text-2xl" />
+            <LinkedinOutlined className="text-2xl" />
+          </div>
+        </div>
+      </div>
+      <div className="text-center border-t border-gray-800 mt-4 pt-4">
+        © Copyright Rimel 2022. All rights reserved.
+      </div>
+    </footer>
+  );
 }
--- src/components/layouts/publicLayout/header.tsx
+++ src/components/layouts/publicLayout/header.tsx
@@ -1,5 +1,78 @@
-import React from "react";
-
+import { Layout, Menu, Input, Button, Drawer } from "antd";
+import {
+  SearchOutlined,
+  HeartOutlined,
+  ShoppingOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import { useState } from "react";
 export default function Header({}) {
-  return <header>header is here</header>;
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const menuStyle = {
+    color: "black",
+    text: "black",
+    // Set the text color to black
+  };
+
+  const menu = (
+    <Menu mode="horizontal" defaultSelectedKeys={["1"]} style={menuStyle}>
+      <Menu.Item
+        className="text-black color-black px-4 py-2 hover:text-black-300"
+        key="1"
+      >
+        Home
+      </Menu.Item>
+      <Menu.Item className="text-black px-4 py-2 hover:text-black-300" key="2">
+        Contact
+      </Menu.Item>
+      <Menu.Item className="text-black px-4 py-2 hover:text-black-100" key="3">
+        About
+      </Menu.Item>
+      <Menu.Item className="text-black px-4 py-2 hover:text-black-300" key="4">
+        Sign Up
+      </Menu.Item>
+    </Menu>
+  );
+
+  return (
+    <header className="bg-blue text-white p-4 grid grid-cols-3 gap-4 max-w-screen-xl mx-auto">
+      <div className="logo text-black font-bold flex justify-self-start">
+        <h5 className="search-input my-4">Exclusive</h5>
+      </div>
+
+      <div className="menu-container flex justify-center mx-2">{menu}</div>
+
+      <div className="search-and-icons flex justify-self-end gap-3">
+        <Input
+          className="search-input my-4"
+          placeholder="What are you looking for?"
+          prefix={<SearchOutlined />}
+        />
+        <Button className=" my-4" icon={<HeartOutlined />} />
+        <Button
+          className="fa fa-shopping-cart  my-4"
+          icon={<ShoppingOutlined />}
+        />
+      </div>
+
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        onClick={onClose}
+        onClose={onClose}
+        visible={visible}
+      >
+        {menu}
+      </Drawer>
+    </header>
+  );
 }
