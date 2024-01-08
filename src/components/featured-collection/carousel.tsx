82ccf2b5deef2cc57696581aa9962d1f64d4efaa
Muhammad Hamza Muneeb <hamza.muneeb@netsoltech.com>
2024-01-08 15:53:36 +0500

    img-Image

--- src/components/featured-collection/carousel.tsx
+++ src/components/featured-collection/carousel.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Carousel } from "antd";
-import { CarouselImage, FacebookIcon } from "@/assets/icons/icons";
 
 export default function MyCarousel() {
   const contentStyle = {
